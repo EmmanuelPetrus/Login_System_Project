a03a26bbe90e50c47df71ed9d502c7f718b0f82c
Emmanuel Peter <peterojonilemi@gmail.com>
2022-06-01 16:41:33 +0100

    first push of header file to repo

---
+++ login.h
@@ -0,0 +1,4 @@
+#define ENTER 13    // ASCII value for enter key
+#define TAB 9       // ASCII value for tab key
+#define BACKSPACE 8 // ASCII value for tab key
+#define BUFFER 25   // Upper bound for characters
